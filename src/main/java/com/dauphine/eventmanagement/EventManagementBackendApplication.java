5018e5d6ff9df2b2df0c76f1166ffaa3d80e46f3
yangyang <yangjoanne216@gmail.com>
2024-06-08 18:37:11 +0200

    first commit,create spring boot structure.

---
+++ src/main/java/com/dauphine/eventmanagement/EventManagementBackendApplication.java
@@ -0,0 +1,14 @@
+package com.dauphine.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementBackendApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(EventManagementBackendApplication.class, args);
+  }
+
+}
